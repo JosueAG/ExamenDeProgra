8845e807c47f0b55ee773ae7bdae190a3d9b129b
Xavier Araya <xaviarsol7@gmail.com>
2016-05-06 09:13:37 -0600

    Examen Progra

---
+++ ExamenDeProgra2/src/Modelo/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author 
+ */
+public class Persona {
+    private String id;
+    private String lugarProcedencia;
+    private String destino;
+    
+    public Persona(String id, String lugarProcedencia, String destino){
+    this.id=id;
+    this.lugarProcedencia= lugarProcedencia;
+    this.destino=destino;
+    }
+/******************************************************************************/
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setLugarProcedencia(String lugarProcedencia) {
+        this.lugarProcedencia = lugarProcedencia;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+/******************************************************************************/
+    public String getId() {
+        return id;
+    }
+
+    public String getLugarProcedencia() {
+        return lugarProcedencia;
+    }
+
+    public String getDestino() {
+        return destino;
+    }    
+}//FIN CLASE PERSONA
